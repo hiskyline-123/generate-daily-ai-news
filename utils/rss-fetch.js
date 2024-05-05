8bbd9a349b742653e9834c9ddd91d2db5af43dbe
hiskyline <hiskyline@evergreenai.cn>
2024-05-05 17:47:42 +0800

    chore: update develop and change sendData import

--- utils/rss-fetch.js
+++ utils/rss-fetch.js
@@ -1,71 +1,23 @@
 const Parser = require('rss-parser');
-const parser = new Parser();
 const moment = require('moment-timezone');
-const axios = require('axios'); // 引入axios库用于发送webhook
-
 const RSS_FEED_URL = "https://rss.app/feeds/LHKHPl2e1aLEJ7eN.xml";
+const axios = require('axios');
 
 const FILTER = {
-  TODAY: 'today',
-  YESTERDAY: 'yesterday',
-  PAST_THREE_DAYS: 'threeDays',
-  PAST_SEVEN_DAYS: 'sevenDays'
+
+    TODAY: 'today',
+
+    YESTERDAY: 'yesterday',
+
+    PAST_THREE_DAYS: 'threeDays',
+
+    PAST_SEVEN_DAYS: 'sevenDays'
+
 };
 
-function isWithinTimeFrame(pubDate, timeFrame) {
-  const publicationDate = moment.tz(pubDate, 'ddd, DD MMM YYYY HH:mm:ss [GMT]', 'GMT');
-  const now = moment.tz(new Date(), 'GMT');
-
-  switch (timeFrame) {
-    case FILTER.TODAY:
-      return publicationDate.isSame(now, 'day');
-    case FILTER.YESTERDAY:
-      return publicationDate.isSame(now.clone().subtract(1, 'days'), 'day');
-    case FILTER.PAST_SEVEN_DAYS:
-        return publicationDate.isAfter(now.clone().subtract(7, 'days').startOf('day'));
-    case FILTER.PAST_THREE_DAYS:
-      return publicationDate.isAfter(now.clone().subtract(3, 'days').startOf('day'));
-    default:
-      return false;
-  }
-}
-
-async function parseRSSGroupedByDate(url, timeFrame) {
-  try {
-    const feed = await parser.parseURL(url);
-    const groupedItems = {};
-
-    feed.items.forEach(item => {
-      if (isWithinTimeFrame(item.pubDate, timeFrame)) {
-        const date = moment(item.pubDate).format('YYYY-MM-DD');
-        if (!groupedItems[date]) {
-          groupedItems[date] = [];
-        }
-        groupedItems[date].push({
-          title: item.title,
-          link: item.link,
-          publicationDate: moment(item.pubDate).format('ddd, DD MMM YYYY HH:mm:ss [GMT]')
-        });
-      }
-    });
-
-    return groupedItems; // 返回分组好的数据
-
-  } catch (error) {
-    console.error('解析RSS feed时发生了错误:', error);
-    throw error; // 将错误向上抛出，让调用者处理
-  }
-}
-
-/**
- * 发送一个webhook请求
- * @param {string} url - webhook的URL地址
- * @param {object} data - 发送到webhook的JSON数据
- * @returns {Promise} - 返回请求的结果
- */
 async function sendWebhook(url, data) {
     try {
-        const response = await axios.post(url, data, {
+        const response = await axios.post(url, { data: data }, {
             headers: {
                 'Content-Type': 'application/json'
             }
@@ -77,12 +29,148 @@ async function sendWebhook(url, data) {
     }
 }
 
-async function processAndSendData() {
-  const groupedData = await parseRSSGroupedByDate(RSS_FEED_URL, FILTER.YESTERDAY);
-//   console.log(groupedData)
-  const webhookUrl = 'https://www.taskade.com/webhooks/flow/01HWH5WASC9A7QRVB2DJPFNMKQ'; // 替换为实际的webhook URL
-  await sendWebhook(webhookUrl, { data: JSON.stringify(groupedData) });
-  console.log('Webhook发送成功');
+
+function decodeHtmlEntities(text) {
+
+    const entityMap = {
+
+        '&amp;': '&',
+
+        '&lt;': '<',
+
+        '&gt;': '>',
+
+        '&quot;': '"',
+
+        '&apos;': "'",
+
+        '&#39;': "'"
+
+    };
+
+    return text.replace(/&amp;|&lt;|&gt;|&quot;|&apos;|&#39;/g, match => entityMap[match]);
+
 }
 
-module.exports =  processAndSendData
+
+
+function isWithinTimeFrame(pubDate, timeFrame) {
+
+    const publicationDate = moment(pubDate);
+
+    const now = moment();
+
+    const yesterday = now.clone().subtract(1, 'days').startOf('day');
+
+
+
+    switch (timeFrame) {
+
+        case FILTER.TODAY:
+
+            return publicationDate.isSame(now, 'day');
+
+        case FILTER.YESTERDAY:
+
+            return publicationDate.isSame(yesterday, 'day');
+
+        case FILTER.PAST_THREE_DAYS:
+
+            return publicationDate.isAfter(now.clone().subtract(3, 'days').startOf('day'));
+
+        case FILTER.PAST_SEVEN_DAYS:
+
+            return publicationDate.isAfter(now.clone().subtract(7, 'days').startOf('day'));
+
+        default:
+
+            return false;
+
+    }
+
+}
+
+
+
+async function parseRSSGroupedByDate(url, timeFrame) {
+
+    const parser = new Parser();
+
+    try {
+
+        const feed = await parser.parseURL(url);
+
+        const groupedItems = {};
+
+
+
+        feed.items.forEach(item => {
+
+            if (isWithinTimeFrame(item.pubDate, timeFrame)) {
+
+                const formattedDate = moment(item.pubDate).format('YYYY-MM-DD');
+
+                if (!groupedItems[formattedDate]) {
+
+                    groupedItems[formattedDate] = [];
+
+                }
+
+                groupedItems[formattedDate].push({
+
+                    title: item.title,
+
+                    link: item.link,
+
+                    publicationDate: item.pubDate,
+
+                    content: decodeHtmlEntities(item.content)
+
+                });
+
+            }
+
+        });
+
+
+
+        return groupedItems;
+
+    } catch (error) {
+
+        console.error('Error parsing RSS feed:', error);
+
+        throw error;
+
+    }
+
+}
+
+
+
+async function processAndSendData() {
+
+    try {
+
+        const groupedData = await parseRSSGroupedByDate(RSS_FEED_URL, FILTER.YESTERDAY);
+
+        // console.log(groupedData)
+
+        const webhookUrl = 'https://www.taskade.com/webhooks/flow/01HWH5WASC9A7QRVB2DJPFNMKQ';
+
+        await sendWebhook(webhookUrl, JSON.stringify(groupedData));
+
+        console.log('Webhook sent successfully');
+
+    } catch (error) {
+
+        console.error('Failed to process and send data:', error);
+
+    }
+
+}
+
+
+
+module.exports = processAndSendData;
+
