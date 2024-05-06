599cdbe95e77c5a4a28883206909e151a6b60d2f
hiskyline <hiskyline@evergreenai.cn>
2024-05-06 10:59:24 +0800

    feat: update response text

--- api/sendData.js
+++ api/sendData.js
@@ -8,7 +8,7 @@ module.exports = async (req, res) => {
     await processAndSendData(); // 调用你的函数
     const date = new Date();
     const beijingTime = new Date(date.getTime() + 8 * 60 * 60 * 1000); // 转换为北京时间
-    res.status(200).send(`Webhook发送成功，北京时间：${beijingTime.toISOString()}`);
+    res.status(200).send(`北京时间：${beijingTime.toISOString()}，Webhook发送成功。`);
   } catch (error) {
     console.error('执行过程中发生错误:', error);
     res.status(500).send('执行失败');
